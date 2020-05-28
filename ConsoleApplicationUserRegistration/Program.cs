f38a5fd836d45f6eac6d6f13efbc45563feb0a2c
Mehboob750 <ms568203@gmail.com>
2020-05-28 09:55:55 +0530

    Welcome to User Registration Application

---
+++ ConsoleApplicationUserRegistration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApplicationUserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ NUnitTestUserRegistration/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace NUnitTestUserRegistration
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
